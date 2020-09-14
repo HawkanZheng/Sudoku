b4dca667918654d8e614a9e65457b39a655a147b
HawkanZheng <hawkan.zheng@gmail.com>
2020-09-13 22:21:13 -0700

    Init commit

---
+++ sudoku_solver.py
@@ -0,0 +1,90 @@
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+
+# Print Sudoku board
+def print_board(brd):
+    for i in range(len(brd)):
+
+        # border for every 3 rows
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - -")
+
+        for j in range(len(brd[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(brd[i][j])
+            else:
+                print(str(brd[i][j]) + " ", end="")
+
+
+# Find empty square
+def find_empty(brd):
+    for i in range(len(brd)):
+        for j in range(len(brd[0])):
+            if brd[i][j] == 0:
+                return (i, j)  # row, column
+
+    return None
+
+
+# Determine if entered number is valid
+def valid(brd, num, pos):
+
+    # Check row
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(brd)):
+        if brd[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check 3x3 square
+    square_x = pos[1] // 3
+    square_y = pos[0] // 3
+    for i in range(square_y * 3, square_y * 3 + 3):
+        for j in range(square_x * 3, square_x * 3 + 3):
+            if brd[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+#Solve board with backtracking, recursively
+def solve(brd):
+
+    #print_board(brd)
+    found = find_empty(brd)
+    if not found:
+        return True
+    else:
+        row, col = found
+
+    for i in range(1, 10):
+        if valid(brd, i, (row, col)):
+            brd[row][col] = i
+
+            if solve(brd):
+                return True
+
+            brd[row][col] = 0
+
+    return False
+
+
+# print_board(board)
+# solve(board)
+# print("                  ")
+# print_board(board)
